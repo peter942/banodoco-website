9b1ea68f52402822471fe0d070fc21c13ebea180
peter942 <petoriaomalley@gmail.com>
2023-02-24 16:13:01 +0100

    Update website.py

--- website.py
+++ website.py
@@ -9,8 +9,16 @@ if 'feature' not in st.session_state:
 
 main_pages = ["home", "philosophy & roadmap", "collaborate", "gallery"]
 
-
 st.set_page_config(page_title="Banodoco")
+
+hide_img_fs = '''
+<style>
+button[title="View fullscreen"]{
+    visibility: hidden;}
+</style>
+'''
+
+st.markdown(hide_img_fs, unsafe_allow_html=True)
 
 
 st.sidebar.image("https://i.ibb.co/xXsd2Cb/Do.png")
